3f22aad0108488c744eb1adcffa61bf7addea1b5
PeterPottR <62199244+PeterPottR@users.noreply.github.com>
2022-02-27 10:23:45 +0100

    02.27

--- app/src/main/java/com/example/ibm/MainActivity.java
+++ app/src/main/java/com/example/ibm/MainActivity.java
@@ -2,19 +2,25 @@ package com.example.ibm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.material.card.MaterialCardView;
 
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    Item[] dataPoints = new Item[0];
+    Item[] dataPoints = new Item[1];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +30,70 @@ public class MainActivity extends AppCompatActivity {
         DataManager dataManager = new DataManager();
 
         dataManager.ImportData("https://android-intern-homework.vercel.app/api");
-        dataPoints = dataManager.items;
+
+        dataPoints[0] = new Item("090206cb-c412-43ec-b11c-b65ecec270f1",
+                "cfuzzens0@economist.com",
+                "kshilleto0",
+                "In sagittis dui vel nisl. Duis ac nibh.",
+                "Traffic Affairs (Mitfahrer)",
+                "https://robohash.org/namrecusandaeet.png?size=50x50&set=set1",
+                "recorded",
+                "2/21/2021",
+                703);
 
         LinearLayout layout = (LinearLayout)findViewById(R.id.main_layout);
-        for (int i =0; i<10; i++)
-        {
-            addCard(layout);
-        }
+
+        addCards(layout);
     }
 
-    public void addCard(LinearLayout parentLayout)
+    public void addCards(LinearLayout parentLayout)
     {
-        MaterialCardView itemCard = new MaterialCardView(this);
-        TextView textView = new TextView(this);
-        textView.setText(dataPoints[0].Guid);
-        itemCard.addView(textView);
-        parentLayout.addView(itemCard);
+        for (int i = 0; i < dataPoints.length; i++) {
+
+            MaterialCardView itemCard = new MaterialCardView(this);
+            TextView titleView = new TextView(this);
+            TextView descView = new TextView(this);
+            titleView.setText(dataPoints[i].Title);
+            descView.setText(dataPoints[i].Description);
+            ImageView main_img = new ImageView();
+
+            new DownloadImageTask(main_img)
+                    .execute("http://java.sogeti.nl/JavaBlog/wp-content/uploads/2009/04/android_icon_256.png");
+
+
+            itemCard.addView(titleView);
+            itemCard.addView(descView);
+
+
+            parentLayout.addView(itemCard);
+        }
+
+
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 
 }
